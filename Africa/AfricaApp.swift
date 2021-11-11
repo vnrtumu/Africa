ba192b643b7e965daad6b32cb43298b242fcb630
venkat reddy <vnr.tumu@gmail.com>
2021-11-12 01:56:37 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by venkat reddy on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by venkat reddy on 12/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
